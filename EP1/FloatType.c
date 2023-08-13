ab5972975534eb0e7ace72bbbcf2c5eb3ecbfe5e
vinícius <v.abreu.fontoura@gmail.com>
2023-08-13 18:38:11 -0300

    [created] auxiliar functions given by the teacher

--- EP1/FloatType.c
+++ EP1/FloatType.c
@@ -2,37 +2,20 @@
  * source: https://randomascii.wordpress.com/2012/01/11/tricks-with-the-floating-point-format/
  */
 
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <float.h>
-#include <math.h>
+#include "FloatType.h"
 
-typedef union
-{
-    int32_t i;
-    float f;
-    struct
-    {   // Bitfields for exploration (32 bits = sign|exponent|mantissa)
-        uint32_t mantissa : 23; // primeiros 23 bits (da direita para a esquerda)
-        uint32_t exponent : 8;  // próximos 8 bits
-        uint32_t sign : 1;      // proximo bit (ou seja, o bit mais a esquerda)
-    } parts;
-} Float_t;
-
-/* imprime a union Float_t como ponto flutuante, hexadecimal e 
+/* imprime a union Float_t como ponto flutuante, hexadecimal e
  * suas partes na forma de inteiros */
-void printFloat_t( Float_t num )
+void printFloat_t(Float_t num)
 {
-   printf("f:%1.9e, ix:0x%08X, s:%d, e:%d, mx:0x%06X\n",
-            num.f, num.i,
-            num.parts.sign, num.parts.exponent, num.parts.mantissa); 
-} 
-
+    printf("f:%1.9e, ix:0x%08X, s:%d, e:%d, mx:0x%06X\n",
+           num.f, num.i,
+           num.parts.sign, num.parts.exponent, num.parts.mantissa);
+}
 
 /* calcula o epsilon relativo a um numero NUM. Ou seja, o valor tal
  * que NUM + epsilon seja > NUM */
-Float_t calculaEpsilonRelativo( Float_t num )
+Float_t calculaEpsilonRelativo(Float_t num)
 {
     // Float_t epsilon = num;
     // epsilon.parts.mantissa += 1;
@@ -41,10 +24,9 @@ Float_t calculaEpsilonRelativo( Float_t num )
     epsilon.f = num.f / 2.0f;
     while (num.f + epsilon.f / 2.0f > num.f)
         epsilon.f /= 2.0f;
-    
+
     return epsilon;
 }
-
 
 int AlmostEqualRelative(Float_t A, Float_t B)
 {
@@ -59,15 +41,14 @@ int AlmostEqualRelative(Float_t A, Float_t B)
 
     printf("\tThe difference: ");
     printFloat_t(diff);
-    
+
     printf("\trel. Epsilon:   ");
     printFloat_t(relEpsilon);
-    
+
     if (diff.f <= relEpsilon.f)
         return 1;
     return 0;
 }
-
 
 int AlmostEqualUlps(Float_t A, Float_t B, int maxULPs)
 {
@@ -79,47 +60,45 @@ int AlmostEqualUlps(Float_t A, Float_t B, int maxULPs)
             return 1;
         return 0;
     }
- 
+
     // Find the difference in ULPs.
     int ulpsDiff = abs(A.i - B.i);
     printf("\tULPs diff: %d\n", ulpsDiff);
-    
+
     if (ulpsDiff < maxULPs)
         return 1;
     return 0;
 }
 
-
 int main()
 {
-   Float_t num, num2;
-   num.f=1.0f;
-   num2.f=1000.0f;
-   
-   /* ***********************************************
-    * Mostra o valor de FLT_EPSILON e calcula o Epsilon relativo
-    * a um numero dado.
-    * Testar com:
-    *   1.0: resultado FLT_EPSILON
-    *   16777215: maior número impar
-    *   16777216: epsilon = 2.0
-    *************************************************/   
+    Float_t num, num2;
+    num.f = 1.0f;
+    num2.f = 1000.0f;
+
+    /* ***********************************************
+     * Mostra o valor de FLT_EPSILON e calcula o Epsilon relativo
+     * a um numero dado.
+     * Testar com:
+     *   1.0: resultado FLT_EPSILON
+     *   16777215: maior número impar
+     *   16777216: epsilon = 2.0
+     *************************************************/
     /*printf("\nEpsilon: %1.15f, Epsilon: %1.9e\n", FLT_EPSILON, FLT_EPSILON);
-   
+
     printf("\nDigite um numero: ");
     scanf("%f", &num.f);
     printf("\n Epsilon: ");
     printFloat_t( calculaEpsilonRelativo(num) );
     /*************************************************/
 
-
-   /******************************************************
-    * Lê um número e imprime o próximo e a diferença entre eles
-    * Testar com:
-    *   1.0: resultado FLT_EPSILON
-    *   16777215: maior número impar
-    *   inf: o próximo é NaN
-    * *****************************************************/
+    /******************************************************
+     * Lê um número e imprime o próximo e a diferença entre eles
+     * Testar com:
+     *   1.0: resultado FLT_EPSILON
+     *   16777215: maior número impar
+     *   inf: o próximo é NaN
+     * *****************************************************/
     /*printf("\nDigite um numero: ");
     scanf("%f", &num.f);
     for (int i=0; i<5; i++)
@@ -131,11 +110,10 @@ int main()
         printFloat_t( num );
         printFloat_t( next );
         printFloat_t( diff );
-      
+
         num = next;
     }
     /*******************************************************/
-
 
     /**************************************************
      * Compara dois numeros usando tres técnicas diferentes
@@ -143,25 +121,25 @@ int main()
      *    1.99999988 e 2.0 (numeros consecutivos)
      *    16777215 e 16777216 (diferença grande mas é a menor possível)
      *    0.000001 e 0.0  (nunca serao iguais em relativo e ULP)
-     * 
+     *
      * ************************************************/
     printf("\nDigite dois numeros reais: ");
     while (scanf("%f %f", &(num.f), &(num2.f)) == 2)
     {
         printf("\nEpsilon: %1.15f, Epsilon: %1.9e\n", FLT_EPSILON, FLT_EPSILON);
-        
+
         printf("\nOs numeros :\n\t");
-        printFloat_t(num); printf("\t");
+        printFloat_t(num);
+        printf("\t");
         printFloat_t(num2);
-        
+
         Float_t diff;
         diff.f = fabs(num.f - num2.f);
-        printf("\n\tSubtraidos: "); 
+        printf("\n\tSubtraidos: ");
         printFloat_t(diff);
-        
+
         printf("\n\n\tComparacao Epsilon Relativo: \n");
         AlmostEqualRelative(num, num2);
-            
 
         printf("\n\n\tComparacao ULPs: \n");
         AlmostEqualUlps(num, num2, 10);
@@ -170,6 +148,5 @@ int main()
     }
     /*******************************************************/
 
-
-   return 0;
+    return 0;
 }
