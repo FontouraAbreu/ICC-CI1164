dfe9bdaad525aad55c975fac4d5c06e1846b1c0f
vinícius <v.abreu.fontoura@gmail.com>
2023-08-17 19:34:29 -0300

    [add] main loop created

--- EP1/main.c
+++ EP1/main.c
@@ -6,11 +6,21 @@ int main()
     char operations[4];
     Float_t floats[5];
     read_expression(operations, floats);
+
+    Interval_t (*current_operation)(Interval_t, Interval_t);
+
     Interval_t *intervals = generate_intervals(floats);
 
+    Interval_t current_result;
+
+    // loop through the operations and store the resulting interval in current_result
     for (int i = 0; i < 4; i++)
     {
-        }
+        current_operation = select_operation(operations[i]);
+        printf("current_operation function pointer: %p\n", current_operation);
+        current_result = current_operation(intervals[i], intervals[i + 1]);
+        printf("\n");
+    }
 
     print_expression(operations, floats);
 
