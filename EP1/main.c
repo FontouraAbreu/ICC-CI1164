53e5590a2c3e5bf295c0b51c207077db1a33af15
Vinicius Fontoura de Abreu <vinicius.fontoura@pop-pr.rnp.br>
2023-08-18 10:16:45 -0300

    [add] printing errors and better printing in code

--- EP1/main.c
+++ EP1/main.c
@@ -12,19 +12,26 @@ int main()
     Interval_t *intervals = generate_intervals(floats);
 
     Interval_t current_result;
-
+    print_expression(operations, floats);
     // loop through the operations and store the resulting interval in current_result
     for (int i = 0; i < 4; i++)
     {
+        printf("%d\n", i + 1);
+
         current_operation = select_operation(operations[i]);
+        printf("[%1.8e,%1.8e] %c [%1.8e,%1.8e] = ", intervals[i].min.f, intervals[i].max.f, operations[i], intervals[i + 1].min.f, intervals[i + 1].max.f);
+
         current_result = current_operation(intervals[i], intervals[i + 1]);
+        printf("[%1.8e,%1.8e]\n", current_result.min.f, current_result.max.f);
+
+        printf("EA: %1.8e; ", absolute_error(current_result).f);
+        printf("ER: %1.8e; ", relative_error(current_result).f);
+        printf("ULPs: %d\n", how_many_ulps_between(current_result));
 
         intervals[i + 1] = current_result;
 
         printf("\n");
     }
-
-    print_expression(operations, floats);
 
     free(intervals);
     return 0;
