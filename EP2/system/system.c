5bdf55137f0b57c610b8de0e8a4b856074c2da33
Vinícius Fontoura <vfa20@inf.ufpr.br>
2023-08-29 14:29:36 -0300

    [wip] adding the 3 main functions

---
+++ EP2/system/system.c
@@ -0,0 +1,22 @@
+#include "system.h"
+
+matrix_t *parcial_pivoting_system_solver(matrix_t *A, matrix_t *b, int *err)
+{
+    matrix_t *x = malloc(sizeof(matrix_t));
+
+    return x;
+}
+
+matrix_t *total_pivoting_system_solver(matrix_t *A, matrix_t *b, int *err)
+{
+    matrix_t *x = malloc(sizeof(matrix_t));
+
+    return x;
+}
+
+matrix_t *refinement_system_solver(matrix_t *A, matrix_t *b, matrix_t *x, int *err)
+{
+    matrix_t *x = malloc(sizeof(matrix_t));
+
+    return x;
+}
--- EP2/system/system.h
+++ EP2/system/system.h
@@ -1,4 +1,9 @@
 #ifndef SYSTEM_H
 #define SYSTEM_H
+#include "../matrix/matrix.h"
+
+matrix_t *parcial_pivoting_system_solver(matrix_t *A, matrix_t *b, int *err);
+matrix_t *total_pivoting_system_solver(matrix_t *A, matrix_t *b, int *err);
+matrix_t *refinement_system_solver(matrix_t *A, matrix_t *b, matrix_t *x, int *err);
 
 #endif // SYSTEM_H
