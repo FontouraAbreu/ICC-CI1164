090771a4d12d2858391da3929d4650f49df8e770
Vinícius Fontoura <vfa20@inf.ufpr.br>
2023-09-05 18:24:03 -0300

    [tweak] using all 3 solvers

--- EP2/main.c
+++ EP2/main.c
@@ -7,7 +7,7 @@ int main()
     printf("\nyour matrix:\n");
     print_matrix(matrix);
     matrix_t *(*current_system_solver)(matrix_t *);
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 3; i++)
     {
         current_system_solver = select_solver(i);
         matrix_t *x = current_system_solver(&matrix);
