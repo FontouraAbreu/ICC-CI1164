8cae6c500019e18008392978f413dd002095db12
vinícius <v.abreu.fontoura@gmail.com>
2023-09-15 23:41:50 -0300

    [wip] implementing lagrange method

--- EP3/interpola/interpola.h
+++ EP3/interpola/interpola.h
@@ -11,6 +11,23 @@ typedef struct {
 
 void newton_method();
 
-void lagrange_method(point_t *table, int n, double x);
+
+/*
+* Will generate an aproximate function based on the table of points given using the lagrange interpolation method
+* @param table: point*
+* @param n: int
+* @param x: double*
+* @return g_x: the aproximated function
+*/
+double *lagrange_method(point_t *table, int n, float x);
+
+float calculate_numerator(point_t *table, int n, float x);
+
+/*
+* Read from stdin a list of N points(point_t)
+* @param n: int(the number of points to be read)
+* @return x:  point_t *
+*/
+point_t *read_points(int n);
 
 #endif // INTERPOLA_H
