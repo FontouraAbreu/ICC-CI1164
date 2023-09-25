f15fbe02e6f0a9073e52917d7e08ad5c592720c8
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2023-09-25 17:32:54 -0300

    [fix] showing both likwid and executable outputs

--- EP3/mflops.sh
+++ EP3/mflops.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+#extracts the argument from the command line
+#and stores it in the variable n
+n=$1
 
 # Define the command to run your executable with likwid-perfctr
-command_to_run="likwid-perfctr -g FLOPS_DP -f ./interpola"
+command_to_run="likwid-perfctr -g FLOPS_DP -f ./interpola $n"
 
 # Execute the command and capture the output
-full_output="$($command_to_run 2>&1)"
+full_output="$($command_to_run)"
+
 
 # Separate the likwid output from the executable output
 likwid_output=$(echo "$full_output" | grep "DP\ \[MFLOP/s\]\ STAT" | head -n1 | awk {'print $12'})
-executable_output=$(echo "$full_output" | grep -v -E '^LIKWID:')
+# extract output beetwen the second "----" and third "----"
+executable_output=$(echo "$full_output" | awk '/^-+$/ {count++; next} count == 2, /Group 1: FLOPS_DP/' | head -n -1)
+
+
+# Print the output
+echo "$executable_output"
+
+# Print the likwid output
+echo "$likwid_output MFLOP/s on average"
