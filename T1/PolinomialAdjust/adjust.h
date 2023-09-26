8a98b5a18365c2f2048117c59b23764f75d45d3b
Vinicius Fontoura de Abreu <vinicius.fontoura@pop-pr.rnp.br>
2023-09-26 09:45:31 -0300

    [add] structs and functions headers

--- T1/PolinomialAdjust/adjust.h
+++ T1/PolinomialAdjust/adjust.h
@@ -1,4 +1,23 @@
 #ifndef POLADJUST_H
 #define POLADJUST_H
+#include "../intervals/intervalar.h"
+
+typedef struct
+{
+    double x;
+    double y;
+} point_t;
+
+typedef struct
+{
+    int size;
+    Interval_t **data;
+    Interval_t *independent_terms;
+    Interval_t *residual;
+} IntervalMatrix_t;
+
+double MinimumSquareMethod(point_t *table, int k, int n);
+
+IntervalMatrix_t *createIntervalMatrix(int n);
 
 #endif // POLADJUST_H
