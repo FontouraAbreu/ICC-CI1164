00f5f0279271f7c8b65855a8f0289b420105adfc
Vinicius Fontoura de Abreu <vinicius.fontoura@pop-pr.rnp.br>
2023-09-26 08:53:33 -0300

    [created] auxiliar files

---
+++ T1/intervals/intervalar.c
@@ -0,0 +1,219 @@
+#include "intervalar.h"
+
+void read_expression(char *operations, Float_t *floats)
+{
+    scanf("%f %c %f %c %f %c %f %c %f", &floats[0].f, &operations[0], &floats[1].f, &operations[1], &floats[2].f, &operations[2], &floats[3].f, &operations[3], &floats[4].f);
+
+    return;
+}
+
+Interval_t *generate_intervals(Float_t *floats)
+{
+    Interval_t *intervals = malloc(sizeof(Interval_t) * 5);
+
+    for (int i = 0; i < 5; i++)
+    {
+
+        intervals[i] = generate_single_interval(&floats[i]);
+    }
+
+    return intervals;
+}
+
+Interval_t generate_single_interval(Float_t *number)
+{
+    Interval_t interval;
+
+    interval.min.f = nextafterf(number->f, -INFINITY);
+    interval.max.f = nextafterf(number->f, INFINITY);
+
+    return interval;
+}
+
+Interval_t (*select_operation(char operation))(Interval_t, Interval_t)
+{
+    switch (operation)
+    {
+    case '+':
+        return op_sum_interval;
+        break;
+    case '-':
+        return op_sub_interval;
+        break;
+    case '*':
+        return op_mul_interval;
+        break;
+    case '/':
+        return op_div_interval;
+        break;
+    default:
+        printf("Invalid operation\n");
+        break;
+    }
+}
+
+void print_expression(char *operations, Float_t *floats)
+{
+    printf("%1.12e %c %1.12e %c %1.12e %c %1.12e %c %1.12e\n", floats[0].f, operations[0], floats[1].f, operations[1], floats[2].f, operations[2], floats[3].f, operations[3], floats[4].f);
+
+    return;
+}
+
+Float_t absolute_error(Interval_t interval)
+{
+    Float_t result;
+
+    result.f = fabs(interval.max.f - interval.min.f);
+
+    return result;
+}
+
+Float_t relative_error(Interval_t interval)
+{
+    Float_t result;
+
+    result.f = fabs(interval.max.f - interval.min.f) / fabs(interval.min.f);
+
+    return result;
+}
+
+int how_many_ulps_between(Interval_t interval)
+{
+    int result;
+
+    result = abs(interval.min.i - interval.max.i) - 1;
+
+    if (result < 0)
+    {
+        return 0;
+    }
+
+    return result;
+}
+
+Interval_t op_sum_interval(Interval_t X, Interval_t Y)
+{
+    Interval_t result;
+    Float_t min, max;
+
+    result.min.f = X.min.f + Y.min.f;
+    result.min.f = nextafterf(result.min.f, -INFINITY);
+    result.max.f = X.max.f + Y.max.f;
+    result.max.f = nextafterf(result.max.f, INFINITY);
+
+    return result;
+}
+
+Interval_t op_sub_interval(Interval_t X, Interval_t Y)
+{
+    Interval_t result;
+    Float_t min, max;
+
+    result.min.f = X.min.f - Y.max.f;
+    result.min.f = nextafterf(result.min.f, -INFINITY);
+    result.max.f = X.max.f - Y.min.f;
+    result.max.f = nextafterf(result.max.f, INFINITY);
+
+    return result;
+}
+
+Interval_t op_div_interval(Interval_t X, Interval_t Y)
+{
+
+    Interval_t result;
+
+    // checking if 0.0 is in Y
+    if (Y.max.f == 0.0 || Y.min.f == 0.0)
+    {
+        result.min.f = -INFINITY;
+        result.max.f = INFINITY;
+
+        return result;
+    }
+
+    Y.min.f = 1 / Y.min.f;
+    Y.max.f = 1 / Y.max.f;
+
+    result = op_mul_interval(X, Y);
+
+    return result;
+}
+
+Interval_t op_mul_interval(Interval_t X, Interval_t Y)
+{
+    Interval_t result;
+    Float_t min, max;
+
+    result.min.f = nextafterf(find_min(X, Y), -INFINITY);
+    result.max.f = nextafterf(find_max(X, Y), INFINITY);
+
+    return result;
+}
+
+float find_min(Interval_t X, Interval_t Y)
+{
+    Float_t min;
+
+    Float_t a = X.min;
+    Float_t b = X.max;
+    Float_t c = Y.min;
+    Float_t d = Y.max;
+
+    float ac = a.f * c.f;
+    float ad = a.f * d.f;
+    float bc = b.f * c.f;
+    float bd = b.f * d.f;
+
+    min.f = ac;
+
+    if (ad < min.f)
+    {
+        min.f = ad;
+    }
+
+    if (bc < min.f)
+    {
+        min.f = bc;
+    }
+
+    if (bd < min.f)
+    {
+        min.f = bd;
+    }
+
+    return min.f;
+}
+
+float find_max(Interval_t X, Interval_t Y)
+{
+    Float_t max;
+
+    Float_t a = X.min;
+    Float_t b = X.max;
+    Float_t c = Y.min;
+    Float_t d = Y.max;
+
+    float ac = a.f * c.f;
+    float ad = a.f * d.f;
+    float bc = b.f * c.f;
+    float bd = b.f * d.f;
+
+    max.f = ac;
+
+    if (ad > max.f)
+    {
+        max.f = ad;
+    }
+
+    if (bc > max.f)
+    {
+        max.f = bc;
+    }
+
+    if (bd > max.f)
+    {
+        max.f = bd;
+    }
+
+    return max.f;
+}
---
+++ T1/intervals/intervalar.h
@@ -0,0 +1,122 @@
+#ifndef INTERVALAR_H
+#define INTERVALAR_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <float.h>
+#include <math.h>
+#include "FloatType.h"
+
+/*
+Atributos
+@param min;
+@param max;
+
+
+@brief
+Seja x um valor real qualquer.
+A representação intervalar de x é dada por X = [min(x), max(x)],
+onde min(x) é o maior número de máquina menor ou igual a x
+e max(x) é o menor número de máquina maior ou igual a x.
+*/
+typedef struct
+{
+    Float_t min;
+    Float_t max;
+} Interval_t;
+
+// Seja X = [a,b] e Y = [c,d]. As operações básicas intervalares são dadas por:
+
+/*
+Seja X = [a,b] e Y = [c,d]
+X + Y = [a+c, b+d]
+*/
+Interval_t op_sum_interval(Interval_t X, Interval_t Y);
+
+/*
+Seja X = [a,b] e Y = [c,d]
+X - Y = [a-d, b-c]
+*/
+Interval_t op_sub_interval(Interval_t X, Interval_t Y);
+
+/*
+Seja X = [a,b] e Y = [c,d]
+X * Y = [a,b] * [c,d]  =  [min{a*c,a*d,b*c,b*d}, max{a*c,a*d,b*c,b*d}]
+*/
+Interval_t op_mul_interval(Interval_t X, Interval_t Y);
+
+/*
+Seja X = [a,b] e Y = [c,d]
+Will find min{a*c,a*d,b*c,b*d}
+*/
+float find_min(Interval_t X, Interval_t Y);
+
+/*
+Seja X = [a,b] e Y = [c,d]
+Will find max{a*c,a*d,b*c,b*d}
+*/
+float find_max(Interval_t X, Interval_t Y);
+
+/*
+Seja X = [a,b] e Y = [c,d]
+X / Y = [a,b] * [1/d,1/c], se 0 não pertence ao intervalo Y
+*/
+Interval_t op_div_interval(Interval_t X, Interval_t Y);
+
+// Auxiliar functions
+
+/*
+This function is used to select the operation to be performed.
+
+For example:
+if char == '+', then select_operation = op_sum_interval
+if char == '-', then select_operation = op_sub_interval
+...
+
+@param char: the operation to be performed. It can be [+,-,*,/]
+
+*/
+// float (*operation)(char);
+Interval_t (*select_operation(char operation))(Interval_t, Interval_t);
+
+Float_t absolute_error(Interval_t interval);
+
+Float_t relative_error(Interval_t interval);
+
+int how_many_ulps_between(Interval_t interval);
+
+/*
+This function will read from stdin the expression to be calculated.
+
+the expression must be in this format:
+
+X1 O1 X2 O2 X3 O3 X4 O4 X5
+
+where Xi is an interval and Oi is an operation [+,-,*,/]
+
+For example:
+
+2.347e-40 * 0.001 + 1.1e+10 - 0.75e-39 / 0.0
+
+@param expressions: a pointer to and array of chars that will store the expression
+@param intervals: a pointer to an array of intervals that will store the intervals
+*/
+void read_expression(char *operations, Float_t *floats);
+void print_expression(char *operations, Float_t *floats);
+
+/*
+Will loop through the array of floats and generate the intervals for each one of them.
+
+@param floats: the array of *Float_t to be converted to intervals
+*/
+Interval_t *generate_intervals(Float_t *floats);
+
+/*
+Will generate a single interval for a given Float_t.
+
+@param number: the Float_t to be converted to an interval
+*/
+Interval_t generate_single_interval(Float_t *number);
+
+#endif
