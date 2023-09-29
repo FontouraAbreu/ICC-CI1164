95335bbf048db5035ccbe75bd65d671f7bef0d9e
vinícius <v.abreu.fontoura@gmail.com>
2023-09-28 21:58:54 -0300

    [wip] least square method function

--- T1/PolinomialAdjust/adjust.c
+++ T1/PolinomialAdjust/adjust.c
@@ -26,6 +26,25 @@ IntervalPoint_t *read_points(int n)
     return points;
 }
 
-double leastSquareMethod(IntervalPoint_t *table, int k, int n) { 
-    
+IntervalMatrix_t *leastSquareMethod(IntervalPoint_t *table, int k, int n) { 
+    IntervalMatrix_t *A = generate_interval_matrix(k+1, k+1);
+    IntervalMatrix_t *B = generate_interval_matrix(k+1, 1);
+    for (int i = 0; i <= k; i++) {
+        for (int j = 0; j <= k; j++) {
+            Interval_t sum = {0.0, 0.0};
+            for (int l = 0; l < n; l++) {
+                sum = op_sum_interval(sum, op_pow_interval(table[l].x, i+j));
+            }
+            A->data[i][j] = sum;
+        }
+        Interval_t sum = {0.0, 0.0};
+        for (int l = 0; l < n; l++) {
+            sum = op_sum_interval(sum, op_mul_interval(table[l].y, op_pow_interval(table[l].x, i)));
+        }
+        B->data[i][0] = sum;
+    }
+    IntervalMatrix_t X = interval_matrix_solve(A, B);
+    interval_matrix_free(A);
+    interval_matrix_free(B);
+    return &X;
 }
--- T1/PolinomialAdjust/adjust.h
+++ T1/PolinomialAdjust/adjust.h
@@ -14,15 +14,8 @@ typedef struct
     Interval_t y;
 } IntervalPoint_t;
 
-typedef struct
-{
-    int size;
-    Interval_t **data;
-    Interval_t *independent_terms;
-    Interval_t *residual;
-} IntervalMatrix_t;
 
-double leastSquareMethod(IntervalPoint_t *table, int k, int n);
+IntervalMatrix_t *leastSquareMethod(IntervalPoint_t *table, int k, int n);
 
 IntervalMatrix_t *createIntervalMatrix(int n);
 
