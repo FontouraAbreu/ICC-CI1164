596af2194c8298f89dd3f4df7c0a9af3ba17e1b8
Vinicius Fontoura de Abreu <vinicius.fontoura@pop-pr.rnp.br>
2023-10-04 09:33:33 -0300

    [change] matrix size from k+1 to k AND now returning Interval Matrix

--- T1/PolinomialAdjust/adjust.c
+++ T1/PolinomialAdjust/adjust.c
@@ -26,36 +26,38 @@ IntervalPoint_t *read_points(int n)
     return points;
 }
 
-Interval_t *leastSquareMethod(IntervalPoint_t *table, int k, int n) { 
+IntervalMatrix_t *leastSquareMethod(IntervalPoint_t *table, int k, int n)
+{
     // Allocate memory for matrices A
-    IntervalMatrix_t *A = generate_interval_matrix(k+1, k+1);
+    IntervalMatrix_t *A = generate_interval_matrix(k, k);
     Float_t zero = {0.0};
     Interval_t zero_interval = generate_single_interval(&zero);
     Interval_t sum;
     // Fill in entries of matrices A
-    for (int i = 0; i <= k; i++) {
+    for (int i = 0; i < k; i++)
+    {
         // fill in entries of matrix A (sum of x^(i+j))
-        for (int j = 0; j <= k; j++) {
+        for (int j = 0; j < k; j++)
+        {
             sum = zero_interval;
-            for (int l = 0; l < n; l++) {
-                sum = op_sum_interval(sum, op_pow_interval(table[l].x, i+j));
+            for (int l = 0; l < n; l++)
+            {
+                sum = op_sum_interval(sum, op_pow_interval(table[l].x, i + j));
             }
             A->data[i][j] = sum;
         }
 
         // fill in independent terms of matrix A (sum of y*x^i)
         sum = zero_interval;
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++)
+        {
             sum = op_sum_interval(sum, op_mul_interval(table[j].y, op_pow_interval(table[j].x, i)));
         }
         A->independent_terms[i] = sum;
     }
 
     // Solve system of linear equations using partial pivoting
-    IntervalMatrix_t *X = partial_pivoting_system_solver(A);
-    show_residual(X, X->independent_terms);
-    
-    // Free memory for matrices A
-    free_intervalMatrix(A);
-    return X->independent_terms;
+    A = partial_pivoting_system_solver(A);
+
+    return A;
 }
