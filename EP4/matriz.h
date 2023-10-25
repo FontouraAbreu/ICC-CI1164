a22dea73e013c851ce99b8879c27e68835e7ccd4
Vinicius Fontoura de Abreu <vinicius.fontoura@pop-pr.rnp.br>
2023-10-25 10:59:10 -0300

    [add] MATxMAT optimized with unroll & jam + blocking [removed] UNROLL_LOOP macro

--- EP4/matriz.h
+++ EP4/matriz.h
@@ -8,40 +8,33 @@
 
 /*optimization factors*/
 #define UF 4
+#define BK 4
 
 /* Macros */
-#define UNROLL_LOOP(UF) \
-  for (int i=0; i < m; i+=UF){ \
-    for (int j=0; j < n; ++j) { \
-      for (int k=0; k < UF; ++k) { \
-        res[i+k] += mat[n*(i+k) + j] * v[j]; \
-      } \
-    } \
-  }
-
-#define ABS(num)  ((num) < 0.0 ? -(num) : (num))
+#define ABS(num) ((num) < 0.0 ? -(num) : (num))
 
 /* Tipos para matrizes e vetores */
 
 typedef double real_t;
 
-typedef real_t * MatRow;
-typedef real_t * Vetor;
+typedef real_t *MatRow;
+typedef real_t *Vetor;
 
 /* ----------- FUNÇÕES ---------------- */
 
-MatRow geraMatRow (int m, int n, int zerar);
-Vetor geraVetor (int n, int zerar);
+MatRow geraMatRow(int m, int n, int zerar);
+Vetor geraVetor(int n, int zerar);
 
-void liberaVetor (void *vet);
+void liberaVetor(void *vet);
 
-void multMatVet (MatRow mat, Vetor v, int m, int n, Vetor res);
+void multMatVet(MatRow mat, Vetor v, int m, int n, Vetor res);
 
-void optimizedMultMatVet_unroll_blocking (MatRow mat, Vetor v, int m, int n, Vetor res);
+void optimizedMultMatVet_unroll_jam_blocking(MatRow mat, Vetor v, int m, int n, Vetor res);
 /*multMatVet optimized using loop_unroll of size 4*/
-void optimizedMultMatVet_unroll (MatRow mat, Vetor v, int m, int n, Vetor res);
+void optimizedMultMatVet_unroll(MatRow mat, Vetor v, int m, int n, Vetor res);
+
 void multMatMat(MatRow A, MatRow B, int n, MatRow C);
+void mulMatMat_unroll_jam_blocking(MatRow A, MatRow B, int n, MatRow C);
 
-void prnMat (MatRow mat, int m, int n);
-void prnVetor (Vetor vet, int n);
-
+void prnMat(MatRow mat, int m, int n);
+void prnVetor(Vetor vet, int n);
