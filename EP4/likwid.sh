117be0a9e102d81ab3f9df3803489b4003ccdb99
Vinicius Fontoura de Abreu <vinicius.fontoura@pop-pr.rnp.br>
2023-10-27 11:40:04 -0300

    [add] saving time spent only once [testing] using static L2CACHE & L3 to test script greps

--- EP4/likwid.sh
+++ EP4/likwid.sh
@@ -7,27 +7,44 @@ SIZES="64 100 128 200 256 512 600 900 1024 2000 2048 3000 4000"
 # likwid groups
 EVENTS="FLOPS_DP ENERGY"
 #CPU core
-CORE=15
+CORE=3
+TIME_SAVED=0
 
 # function to parse the output based on the metric needed using switch case
 function parse_output() {
     case $1 in
         FLOPS_DP)
             likwid_output=$(cat $g_$n.txt | grep "DP\ \[MFLOP/s\]" | awk 'NR%3==1 {dp = $(NF-1)} NR%3==2 {avx_dp = $(NF-1); printf "%s %s ", dp, avx_dp}')
-            time_spent=$(cat $g_$n.txt | awk '/^-+$/ {count++; if (count == 2) flag=1; else flag=0; next} flag && !/^-+$/ {printf "%s ", $0} flag && /^-+$/ {exit}')
+            
+            # if time saved == 1, do not save the time spent
+            if [ $TIME_SAVED == 1 ]; then
+                time_spent=$(cat $g_$n.txt | awk '/^-+$/ {count++; if (count == 2) flag=1; else flag=0; next} flag && !/^-+$/ {printf "%s ", $0} flag && /^-+$/ {exit}')
+            fi
             ;;
         ENERGY)
             likwid_output=$(cat $g_$n.txt | grep "Energy\ \[J\]\ " | awk {'print $5'} | tr '\n' ' ')
-            time_spent=$(cat $g_$n.txt | awk '/^-+$/ {count++; if (count == 2) flag=1; else flag=0; next} flag && !/^-+$/ {printf "%s ", $0} flag && /^-+$/ {exit}')
+            
+            # if time saved == 1, do not save the time spent
+            if [ $TIME_SAVED == 1 ]; then
+                time_spent=$(cat $g_$n.txt | awk '/^-+$/ {count++; if (count == 2) flag=1; else flag=0; next} flag && !/^-+$/ {printf "%s ", $0} flag && /^-+$/ {exit}')
+            fi
             ;;
-        MEM)
-            likwid_output=$(echo "$g_$n.txt" | grep "MEM\ \[MBytes/s\]\ STAT" | head -n1 | awk {'print $12'})
-            time_spent=$(cat $g_$n.txt | awk '/^-+$/ {count++; if (count == 2) flag=1; else flag=0; next} flag && !/^-+$/ {printf "%s ", $0} flag && /^-+$/ {exit}')
+        L2CACHE)
+            likwid_output=$(cat "L2Cache.txt" | grep "\ miss \ratio\ " | awk '{print $(NF-1)}' | tr '\n' ' ')
+            
+            # if time saved == 1, do not save the time spent
+            if [ $TIME_SAVED == 1 ]; then
+                time_spent=$(cat $g_$n.txt | awk '/^-+$/ {count++; if (count == 2) flag=1; else flag=0; next} flag && !/^-+$/ {printf "%s ", $0} flag && /^-+$/ {exit}')
+            fi
             ;;
         # cache miss ratio
-        CACHE)
-            likwid_output=$(echo "$g_$n.txt" | grep "cache\ miss\ ratio\ \[\%\]\ STAT" | head -n1 | awk {'print $12'})
-            time_spent=$(cat $g_$n.txt | awk '/^-+$/ {count++; if (count == 2) flag=1; else flag=0; next} flag && !/^-+$/ {printf "%s ", $0} flag && /^-+$/ {exit}')
+        L3)
+            likwid_output=$(cat "$g_$n.txt" | grep "L3\ bandwidth\ " | awk '{print $(NF-1)}' | tr '\n' ' ')
+
+            # if time saved == 1, do not save the time spent
+            if [ $TIME_SAVED == 1 ]; then
+                time_spent=$(cat $g_$n.txt | awk '/^-+$/ {count++; if (count == 2) flag=1; else flag=0; next} flag && !/^-+$/ {printf "%s ", $0} flag && /^-+$/ {exit}')
+            fi
             ;;
         *)
             echo "Invalid metric"
@@ -36,22 +53,24 @@ function parse_output() {
 }
 
 # creating time.csv only once
-echo "N,MultMatVet,OptimizedMultMatVet,MultMatMat,OptmizedMultMatMat" > time.csv
+echo "N MultMatVet OptimizedMultMatVet MultMatMat OptmizedMultMatMat" > time.csv
 
 # for each group, run the executable for each size and parse the output
 for g in $EVENTS; do
     if [ -f $g.csv ]; then
         rm $g.csv
     fi
-    echo "N,MultMatVet,OptimizedMultMatVet,MultMatMat,OptmizedMultMatMat" > $g.csv
+    echo "N MultMatVet OptimizedMultMatVet MultMatMat OptmizedMultMatMat" > $g.csv
 
     for n in $SIZES; do
         likwid-perfctr -C $CORE -g $g -m -f ./$EXECUTABLE $n > $g_$n.txt
         parse_output $g $n
         echo "$n, $likwid_output" >> $g.csv
         # if is the first group, save the time spent
-        if [ $g == "FLOPS_DP" ]; then
+        first_group=$(echo $EVENTS | awk '{print $1}')
+        if [ $g == $first_group ]; then
             echo "$n, $time_spent" >> time.csv
+            TIME_SAVED=1
         fi
         rm $g_$n.txt
     done
