aa59841775ca9fa9f59f97aa745025514ffc9568
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2023-11-03 20:08:40 -0300

    [wip] adding optmized functions tests

--- T2/main.c
+++ T2/main.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 
     // reading inputs
     int n, k;
-    double time1, time2;
+    double timeLS_non_optmized, timeSS_non_optmized, timeLS_optmized, timeSS_optmized;
     if (scanf("%d", &n) != 1)
     {
         fprintf(stderr, "Erro ao ler n\n");
@@ -23,19 +23,19 @@ int main(int argc, char *argv[])
     }
 
 
-    time1 = timestamp();
+    timeLS_non_optmized = timestamp();
     LIKWID_MARKER_START("LEAST_SQUARE_METHOD");
     IntervalPoint_t *table = read_points(k);
     IntervalMatrix_t *coefficients_matrix = leastSquareMethod(table, k, n);
     LIKWID_MARKER_STOP("LEAST_SQUARE_METHOD");
-    time1 = timestamp()- time1;
+    timeLS_non_optmized = timestamp()- timeLS_non_optmized;
 
-    time2 = timestamp();
+    timeSS_non_optmized = timestamp();
     LIKWID_MARKER_START("SYSTEM_SOLVER");
     IntervalMatrix_t *triangular_matrix = partial_pivoting_system_solver(coefficients_matrix);
     Interval_t *solution = retrossubs(triangular_matrix);
     LIKWID_MARKER_STOP("SYSTEM_SOLVER");
-    time2 = timestamp() - time2;
+    timeSS_non_optmized = timestamp() - timeSS_non_optmized;
 
     Interval_t *residual = show_residual(coefficients_matrix, solution, table, k);
 
@@ -48,8 +48,8 @@ int main(int argc, char *argv[])
         printf("[%1.8e, %1.8e] ", residual[i].min.f, residual[i].max.f);
     printf("\n");
 
-    printf("%1.8e\n", time1);
-    printf("%1.8e\n", time2);
+    printf("%1.8e\n", timeLS_non_optmized);
+    printf("%1.8e\n", timeSS_non_optmized);
 
     LIKWID_MARKER_CLOSE;
     return 0;
