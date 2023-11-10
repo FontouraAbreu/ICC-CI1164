ea68fb269f73ff035038bf55e6566727351e80be
Vinícius Fontoura <vfa20@inf.ufpr.br>
2023-11-10 17:32:40 -0300

    [add] likwid lib checkers

--- T2/main.c
+++ T2/main.c
@@ -1,11 +1,15 @@
 #include "utils/utils.h"
 #include "PolinomialAdjust/adjust.h"
 #include "system/system.h"
-#include "likwid.h"
+#ifdef LIKWID_PERFMON
+    #include "likwid.h"
+#endif
 
 int main(int argc, char *argv[])
 {
-    LIKWID_MARKER_INIT;
+    #ifdef LIKWID_PERFMON
+        LIKWID_MARKER_INIT;
+    #endif
 
     // reading inputs
     lli n, k;
@@ -25,34 +29,52 @@ int main(int argc, char *argv[])
 
     // NON OPTMIZED VERSION
     timeLS_non_optmized = timestamp();
-    //LIKWID_MARKER_START("LEAST_SQUARE_METHOD");
+    #ifdef LIKWID_PERFMON
+        LIKWID_MARKER_START("LEAST_SQUARE_METHOD");
+    #endif
     IntervalPoint_t *table = read_points(k);
     IntervalMatrix_t *coefficients_matrix = leastSquareMethod(table, k, n);
-    //LIKWID_MARKER_STOP("LEAST_SQUARE_METHOD");
+    print_matrix(coefficients_matrix);
+    #ifdef LIKWID_PERFMON
+        LIKWID_MARKER_STOP("LEAST_SQUARE_METHOD");
+    #endif
     timeLS_non_optmized = timestamp()- timeLS_non_optmized;
 
     timeSS_non_optmized = timestamp();
-    //LIKWID_MARKER_START("SYSTEM_SOLVER");
+    #ifdef LIKWID_PERFMON
+        LIKWID_MARKER_START("SYSTEM_SOLVER");
+    #endif
     IntervalMatrix_t *triangular_matrix = partial_pivoting_system_solver(coefficients_matrix);
     Interval_t *solution = retrossubs(triangular_matrix);
-    //LIKWID_MARKER_STOP("SYSTEM_SOLVER");
+    #ifdef LIKWID_PERFMON
+        LIKWID_MARKER_STOP("SYSTEM_SOLVER");
+    #endif
     timeSS_non_optmized = timestamp() - timeSS_non_optmized;
 
     Interval_t *residual = show_residual(coefficients_matrix, solution, table, k);
 
     // OPTMIZED VERSION
     timeLS_optmized = timestamp();
-    //LIKWID_MARKER_START("LEAST_SQUARE_METHOD_OPTMIZED");
+    #ifdef LIKWID_PERFMON
+        LIKWID_MARKER_START("LEAST_SQUARE_METHOD_OPTMIZED");
+    #endif
     OptIntervalPoint_t optTable = optRead_points(table, k);
     OptIntervalMatrix_t *optCoefficients_matrix = optLeastSquareMethod(optTable, k, n);
-    //LIKWID_MARKER_STOP("LEAST_SQUARE_METHOD_OPTMIZED");
+    print_opt_matrix(optCoefficients_matrix);
+    #ifdef LIKWID_PERFMON
+        LIKWID_MARKER_STOP("LEAST_SQUARE_METHOD_OPTMIZED");
+    #endif
     timeLS_optmized = timestamp() - timeLS_optmized;
 
     //timeSS_optmized = timestamp();
-    //LIKWID_MARKER_START("SYSTEM_SOLVER_OPTMIZED");
+    #ifdef LIKWID_PERFMON
+        LIKWID_MARKER_START("SYSTEM_SOLVER_OPTMIZED");
+    #endif
     //OptIntervalMatrix_t *optTriangular_matrix = optPartial_pivoting_system_solver(optCoefficients_matrix);
     //Interval_t *optSolution = optRetrossubs(optTriangular_matrix);
-    //LIKWID_MARKER_STOP("SYSTEM_SOLVER_OPTMIZED");
+    #ifdef LIKWID_PERFMON
+        LIKWID_MARKER_STOP("SYSTEM_SOLVER_OPTMIZED");
+    #endif
     //timeSS_optmized = timestamp() - timeSS_optmized;
 
 
@@ -64,6 +86,8 @@ int main(int argc, char *argv[])
     printf("%1.8e\n", timeLS_non_optmized);
     printf("%1.8e\n", timeSS_non_optmized);
 
-    LIKWID_MARKER_CLOSE;
+    #ifdef LIKWID_PERFMON
+        LIKWID_MARKER_CLOSE;
+    #endif
     return 0;
 }
